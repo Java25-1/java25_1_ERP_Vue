417d320b24d9270be5db68109f90f11683eb476a
SunLueMing <jioubidao@163.com>
2019-08-30 18:05:47 +0800

    2019.08.30

--- src/views/账款管理/Kysk.vue
+++ src/views/账款管理/Kysk.vue
@@ -5,15 +5,15 @@
                 <el-col :span="10"><div class="grid-content bg-purple" :label-position="left">
 
                     <el-form-item label="客户：">
-                        <el-input v-model="form.name"></el-input>
+                        <el-input v-model="form.Cliid"></el-input>
                     </el-form-item>
                     <el-form-item  label="结算方式一：">
                         <el-row>
                             <el-col :span="12"><div class="grid-content bg-purple">
-                                <el-input v-model="form.name" ></el-input>
+                                <el-input v-model="form.ReckonTypeF" ></el-input>
                             </div></el-col>
                             <el-col :span="12"><div class="grid-content bg-purple-light">
-                                <el-input v-model="form.name" ></el-input>
+                                <el-input v-model="form.ReckonMoneyF" ></el-input>
                             </div></el-col>
                         </el-row>
 
@@ -22,10 +22,10 @@
                     <el-form-item  label="结算方式二：">
                         <el-row>
                             <el-col :span="12"><div class="grid-content bg-purple">
-                                <el-input v-model="form.name" ></el-input>
+                                <el-input v-model="form.ReckonTypeS" ></el-input>
                             </div></el-col>
                             <el-col :span="12"><div class="grid-content bg-purple-light">
-                                <el-input v-model="form.name" ></el-input>
+                                <el-input v-model="form.ReckonMoneyS" ></el-input>
                             </div></el-col>
                         </el-row>
                         <!--                    <el-input v-model="form.name"></el-input>-->
@@ -33,24 +33,20 @@
                     <el-form-item  label="结算方式三：">
                         <el-row>
                             <el-col :span="12"><div class="grid-content bg-purple">
-                                <el-input v-model="form.name" ></el-input>
+                                <el-input v-model="form.ReckonTypeT" ></el-input>
                             </div></el-col>
                             <el-col :span="12"><div class="grid-content bg-purple-light">
-                                <el-input v-model="form.name" ></el-input>
+                                <el-input v-model="form.ReckonMoneyT" ></el-input>
                             </div></el-col>
                         </el-row>
                         <!--                    <el-input v-model="form.name"></el-input>-->
                     </el-form-item>
                     <el-form-item  label="折扣率：">
-                        <el-input v-model="form.name"></el-input>
+                        <el-input v-model="form.GatherDiscount"></el-input>
                     </el-form-item>
                     <el-form-item  label="终止日期：">
-                        <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+                        <el-date-picker type="date" placeholder="选择日期" v-model="form.GatherendaccountMonth" style="width: 100%;"></el-date-picker>
                     </el-form-item>
-
-                    <!--                <el-form-item>-->
-                    <!--                    <el-button type="primary" @click="onSubmit">立即创建</el-button>-->
-                    <!--                </el-form-item>-->
 
                 </div></el-col>
                 <el-col :span="1"><div class="grid-content bg-purple-light"></div></el-col>
@@ -59,49 +55,78 @@
                         <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
                     </el-form-item>
                     <el-form-item  label="单据号码：">
-                        <el-input v-model="form.name"></el-input>
+                        <el-input v-model="form.billId"></el-input>
                     </el-form-item>
                     <el-form-item  label="币种：">
-                        <el-input v-model="form.name"></el-input>
+                        <el-input v-model="form.currencyType"></el-input>
                     </el-form-item>
                     <el-form-item  label="汇率：">
-                        <el-input v-model="form.name"></el-input>
+                        <el-input v-model="form.exchangerate"></el-input>
                     </el-form-item>
                     <el-form-item  label="应收冲款类型：">
                         <el-input v-model="form.TypesofWithdrawals" ></el-input>
                     </el-form-item>
                     <el-form-item  label="凭证编号：">
-                        <el-input v-model="form.name"></el-input>
+                        <el-input v-model="form.Proofnumber"></el-input>
                     </el-form-item>
                 </div></el-col>
             </el-row>
             <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
                 <el-tab-pane label="内容" name="first">
                     <el-table
-                            :data="tableData3"
+                            :data="sorlist"
                             style="width: 100%"
                             height="250">
-                        <el-table-column
-                                fixed
-                                prop="date"
-                                label="预收金额"
-                                width="200">
-                        </el-table-column>
-                        <el-table-column
-                                prop="name"
-                                label="来源别"
-                                width="200">
-                        </el-table-column>
-                        <el-table-column
-                                prop="province"
-                                label="来源单号"
-                                width="420">
-                        </el-table-column>
-                        <el-table-column
-                                prop="city"
-                                label="摘要"
-                                width="420">
-                        </el-table-column>
+
+
+                            <el-table-column
+                                    label='预收金额'
+                                    class-name="column-bg-color-editable"
+                                    width="300"
+                                    show-overflow-tooltip>
+                                <template slot-scope="scope">
+                                    <div class="input-box">
+                                        <el-input size="small" @blur="handleInputBlur" v-model="scope.row.promisedmoney" ></el-input>
+                                    </div>
+
+                                </template>
+                            </el-table-column>
+                            <el-table-column
+                                    label='来源别'
+                                    class-name="column-bg-color-editable"
+                                    width="300"
+                                    show-overflow-tooltip>
+                                <template slot-scope="scope">
+                                    <div class="input-box">
+                                        <el-input size="small" @blur="handleInputBlur" v-model="scope.row.sinname" ></el-input>
+                                    </div>
+
+                                </template>
+                            </el-table-column>
+                            <el-table-column
+                                    label='来源单号'
+                                    class-name="column-bg-color-editable"
+                                    width="300"
+                                    show-overflow-tooltip>
+                                <template slot-scope="scope">
+                                    <div class="input-box">
+                                        <el-input size="small" @blur="handleInputBlur" v-model="scope.row.originbillid" ></el-input>
+                                    </div>
+
+                                </template>
+                            </el-table-column>
+                            <el-table-column
+                                    label='摘要'
+                                    class-name="column-bg-color-editable"
+                                    width="300"
+                                    show-overflow-tooltip>
+                                <template slot-scope="scope">
+                                    <div class="input-box">
+                                        <el-input size="small" @blur="handleInputBlur" v-model="scope.row.summary" ></el-input>
+                                    </div>
+
+                                </template>
+                            </el-table-column>
                     </el-table>
 
                 </el-tab-pane>
@@ -176,56 +201,7 @@
         data() {
             return {
                 activeName2: 'first',
-                tableData3: [{
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-08',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-06',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    province: '上海',
-                    city: '普陀区',
-                    address: '上海市普陀区金沙江路 1518 弄',
-                    zip: 200333
-                }],
+
                 options: [{
                     value: '选项1',
                     label: '黄金糕'
@@ -241,29 +217,73 @@
                 }, {
                     value: '选项5',
                     label: '北京烤鸭'
-                }], value8: '', value7: '',
+                }]
+                , value8: '',
+                value7: '',
                 form: {
-                    name: '',
+                    currencyType: 'RMB',
                     region: '',
                     TypesofWithdrawals:'预收款',
                     date1: '',
-                    date2: '',
+                    exchangerate: '1:1',
                     delivery: false,
                     type: [],
                     resource: '',
                     desc: '',
+                    Proofnumber:'1',
                     activeName: 'second'
-                }
+                },
+                sorlist:[]
             }
         },
         methods: {
             onSubmit() {
                 console.log('submit!');
+            },onSubmit() {
+                console.log('submit!');
+            },
+            loginDetail(biilid){
+                let _this = this;
+              this.$axios.get(`http://127.0.0.1:8080/Advancegatherdetails/SelAdvDetail/${biilid}`).then(resp =>{
+                  _this.sorlist =resp.data;
+              });
+            },
+            //单元格点击后，显示input，并让input 获取焦点
+            handleCellClick:function(row, column, cell, event){
+                emptransfer.addClass(cell,'current-cell');
+                if(emptransfer.getChildElement(cell,3) !== 0){
+                    var _inputParentNode =emptransfer.getChildElement(cell,3);
+                    if(_inputParentNode.hasChildNodes()&& _inputParentNode.childNodes.length > 2) {
+                        var _inputNode = _inputParentNode.childNodes[2];
+                        if(_inputNode.tagName === 'INPUT'){
+                            _inputNode.focus();
+                        }
+                    }
+                }
+            },
+//input框失去焦点事件
+            handleInputBlur:function(event){   //当 input 失去焦点 时,input 切换为 span，并且让下方 表格消失（注意，与点击表格事件的执行顺序）
+                var _event = event;
+                setTimeout(function(){
+                    var _inputNode = _event.target;
+                    if(emptransfer.getParentElement(_inputNode,4)!==0){
+                        var _cellNode = emptransfer.getParentElement(_inputNode,4);
+                        emptransfer.removeClass(_cellNode,'current-cell');
+                        emptransfer.removeClass(_cellNode,'current-cell2');
+                    }
+                },200);
             }
             // },
             // handleClick(tab, event) {
             //     console.log(tab, event);
             // }
+            // },
+            // handleClick(tab, event) {
+            //     console.log(tab, event);
+            // }
+        },
+        mounted(){
+            this.loginDetail("001");
         }
     }
 </script>
